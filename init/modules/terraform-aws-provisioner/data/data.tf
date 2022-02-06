427d4881e427bd1582c9b072ed12dee688e45eec
Andrew Graham <queglay@gmail.com>
2022-02-06 22:39:35 +1030

    cleanup

--- init/modules/terraform-aws-provisioner/data/data.tf
+++ init/modules/terraform-aws-provisioner/data/data.tf
@@ -4,12 +4,3 @@
 provider "aws" {}
 
 data "aws_region" "current" {}
-
-# data "terraform_remote_state" "user_data" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
-#   backend = "s3"
-#   config = {
-#     bucket = "state.terraform.${var.bucket_extension}"
-#     key    = "firehawk-render-cluster/modules/terraform-aws-user-data-rendernode/module/terraform.tfstate"
-#     region = data.aws_region.current.name
-#   }
-# }
--- init/modules/terraform-aws-provisioner/data/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "user_data_base64" {
-  value = try(data.terraform_remote_state.user_data.outputs.user_data_base64 , null)
-}
