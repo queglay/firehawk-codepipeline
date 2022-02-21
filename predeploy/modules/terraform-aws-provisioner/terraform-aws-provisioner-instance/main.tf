089b887e1a4512593c55c3fefd258615f73a9a34
Andrew Graham <queglay@gmail.com>
2022-02-21 13:57:55 +1030

    update max revisions for codedeploy

--- predeploy/modules/terraform-aws-provisioner/terraform-aws-provisioner-instance/main.tf
+++ predeploy/modules/terraform-aws-provisioner/terraform-aws-provisioner-instance/main.tf
@@ -18,15 +18,15 @@ data "terraform_remote_state" "provisioner_profile" { # read the arn with data.t
 }
 locals {
   provisioner_tags = merge(var.common_tags, {
-    name  = var.name
-    role  = "provisioner"
-    route = "public"
+    name             = var.name
+    role             = "provisioner"
+    route            = "public"
     deployment_group = "firehawk-provisioner-deploy-group"
   })
-  public_ip       = element(concat(aws_instance.provisioner.*.public_ip, list("")), 0)
-  private_ip      = element(concat(aws_instance.provisioner.*.private_ip, list("")), 0)
-  public_dns      = element(concat(aws_instance.provisioner.*.public_dns, list("")), 0)
-  id              = element(concat(aws_instance.provisioner.*.id, list("")), 0)
+  public_ip           = element(concat(aws_instance.provisioner.*.public_ip, list("")), 0)
+  private_ip          = element(concat(aws_instance.provisioner.*.private_ip, list("")), 0)
+  public_dns          = element(concat(aws_instance.provisioner.*.public_dns, list("")), 0)
+  id                  = element(concat(aws_instance.provisioner.*.id, list("")), 0)
   provisioner_address = var.route_public_domain_name ? "provisioner.${var.public_domain_name}" : local.public_ip
 }
 resource "aws_instance" "provisioner" {
@@ -35,30 +35,17 @@ resource "aws_instance" "provisioner" {
   instance_type          = var.instance_type
   subnet_id              = tolist(var.public_subnet_ids)[0]
   tags                   = merge(map("Name", var.name), local.provisioner_tags)
-  # user_data              = data.template_file.user_data_auth_client.rendered
+  user_data              = data.template_file.user_data_provisioner.rendered
   iam_instance_profile   = try(data.terraform_remote_state.provisioner_profile.outputs.instance_profile_name, null)
-  vpc_security_group_ids = [ try(data.terraform_remote_state.provisioner_security_group.outputs.security_group_id, null) ]
+  vpc_security_group_ids = [try(data.terraform_remote_state.provisioner_security_group.outputs.security_group_id, null)]
   root_block_device {
     delete_on_termination = true
-    volume_size = 24
+    volume_size           = 24
   }
 }
-# data "template_file" "user_data_auth_client" {
-#   template = file("${path.module}/user-data-auth-ssh-host-iam.sh")
-#   vars = {
-#     consul_cluster_tag_key   = var.consul_cluster_tag_key
-#     consul_cluster_tag_value = var.consul_cluster_name
-#     aws_internal_domain      = var.aws_internal_domain
-#     aws_external_domain      = var.aws_external_domain
-#     example_role_name        = "provisioner-vault-role"
-#     vault_token              = ""
-#   }
-# }
-# resource "aws_route53_record" "provisioner_record" {
-#   count   = var.route_public_domain_name && var.create_vpc ? 1 : 0
-#   zone_id = element(concat(list(var.route_zone_id), list("")), 0)
-#   name    = element(concat(list("provisioner.${var.public_domain_name}"), list("")), 0)
-#   type    = "A"
-#   ttl     = 300
-#   records = [local.public_ip]
-# }
+data "template_file" "user_data_provisioner" {
+  template = file("${path.module}/user-data-provisioner.sh")
+  vars = {
+    max_revisions = 2 # the number of code revisions to store on the instance
+  }
+}
---
+++ predeploy/modules/terraform-aws-provisioner/terraform-aws-provisioner-instance/user-data-provisioner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Configure max revisions for codedeploy-agent..."
+
+sed '$ d' /etc/codedeploy-agent/conf/codedeployagent.yml > /etc/codedeploy-agent/conf/temp.yml
+echo ':max_revisions: ${max_revisions}' >> /etc/codedeploy-agent/conf/temp.yml
+rm -f /etc/codedeploy-agent/conf/codedeployagent.yml
+mv /etc/codedeploy-agent/conf/temp.yml /etc/codedeploy-agent/conf/codedeployagent.yml
+service codedeploy-agent restart
