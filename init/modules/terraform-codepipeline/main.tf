6d469bd13b09045874636e69107b93281b931751
Andrew Graham <queglay@gmail.com>
2022-03-10 21:39:48 +1030

    format json

--- init/modules/terraform-codepipeline/main.tf
+++ init/modules/terraform-codepipeline/main.tf
@@ -120,50 +120,50 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
 
   policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Effect":"Allow",
-        "Action": [
-            "s3:GetObject",
-            "s3:GetObjectVersion",
-            "s3:GetBucketVersioning",
-            "s3:PutObjectAcl",
-            "s3:PutObject"
-        ],
-        "Resource": [
-            "${aws_s3_bucket.codepipeline_bucket.arn}",
-            "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
-    },
-    {
-        "Effect": "Allow",
-        "Action": [
-            "codestar-connections:UseConnection"
-        ],
-        "Resource": "${aws_codestarconnections_connection.my_github_connection.arn}"
-    },
-    {
-        "Effect": "Allow",
-        "Action": [
-            "codebuild:BatchGetBuilds",
-            "codebuild:StartBuild"
-        ],
-        "Resource": "*"
-    },
-    {
-        "Action": [
-            "codedeploy:CreateDeployment",
-            "codedeploy:GetApplication",
-            "codedeploy:GetApplicationRevision",
-            "codedeploy:GetDeployment",
-            "codedeploy:GetDeploymentConfig",
-            "codedeploy:RegisterApplicationRevision"
-        ],
-        "Resource": "*",
-        "Effect": "Allow"
-    },
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetBucketVersioning",
+                "s3:PutObjectAcl",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.codepipeline_bucket.arn}",
+                "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codestar-connections:UseConnection"
+            ],
+            "Resource": "${aws_codestarconnections_connection.my_github_connection.arn}"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codebuild:BatchGetBuilds",
+                "codebuild:StartBuild"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codedeploy:CreateDeployment",
+                "codedeploy:GetApplication",
+                "codedeploy:GetApplicationRevision",
+                "codedeploy:GetDeployment",
+                "codedeploy:GetDeploymentConfig",
+                "codedeploy:RegisterApplicationRevision"
+            ],
+            "Resource": "*"
+        }
+    ]
 }
 EOF
 }
