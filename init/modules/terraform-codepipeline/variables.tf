304ec26b82501fe5974bd6c5e8b73bd7aa5b63d5
Andrew Graham <queglay@gmail.com>
2022-03-11 13:37:59 +1030

    Add missing var

--- init/modules/terraform-codepipeline/variables.tf
+++ init/modules/terraform-codepipeline/variables.tf
@@ -2,3 +2,7 @@ variable "bucket_extension" {
   description = "The extension for cloud storage used to label your S3 storage buckets (eg: example.com, my-name-at-gmail.com). This can be any unique name (it must not be taken already, globally).  commonly, it is a domain name you own, or an abbreviated email adress.  No @ symbols are allowed. See this doc for naming restrictions on s3 buckets - https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html"
   type        = string
 }
+variable "resourcetier" {
+  description = "The resource tier uniquely defining the deployment area.  eg: dev/green/blue/main"
+  type        = string
+}
