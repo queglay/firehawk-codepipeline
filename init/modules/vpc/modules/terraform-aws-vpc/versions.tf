8f46b59ca147172e8c127e159dfde7fd753d55c4
Andrew Graham <queglay@gmail.com>
2022-03-22 18:24:02 +1030

    provide version for null

--- init/modules/vpc/modules/terraform-aws-vpc/versions.tf
+++ init/modules/vpc/modules/terraform-aws-vpc/versions.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.15.0"
     }
     null = {
       source = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
   required_version = ">= 0.13"
