d63775a6f1a9cd168befae17e93d5fb813295452
Andrew Graham <queglay@gmail.com>
2022-03-27 18:43:40 +1030

    mising local

--- predeploy/modules/terraform-aws-provisioner/terraform-aws-provisioner-instance/main.tf
+++ predeploy/modules/terraform-aws-provisioner/terraform-aws-provisioner-instance/main.tf
@@ -17,6 +17,7 @@ data "terraform_remote_state" "provisioner_profile" { # read the arn with data.t
   }
 }
 locals {
+  volume_size = 96
   provisioner_tags = merge(var.common_tags, {
     name             = var.name
     role             = "provisioner"
@@ -34,7 +35,7 @@ resource "aws_instance" "provisioner" {
   ami                    = var.provisioner_ami_id
   instance_type          = var.instance_type
   subnet_id              = tolist(var.public_subnet_ids)[0]
-  tags                   = merge(tomap({"Name": var.name}), local.provisioner_tags)
+  tags                   = merge(tomap({ "Name" : var.name }), local.provisioner_tags)
   user_data              = data.template_file.user_data_provisioner.rendered
   iam_instance_profile   = try(data.terraform_remote_state.provisioner_profile.outputs.instance_profile_name, null)
   vpc_security_group_ids = [try(data.terraform_remote_state.provisioner_security_group.outputs.security_group_id, null)]
