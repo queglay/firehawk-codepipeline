6e92b0b6380f5d135d155bdac25a858277064918
Andrew Graham <queglay@gmail.com>
2022-05-06 17:09:26 +0930

    Provide usage

--- delete-all-old-amis.sh
+++ delete-all-old-amis.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+
+function print_usage {
+  echo
+  echo "Usage: ./delete-all-old-amis.sh [OPTIONS]"
+  echo
+  echo "This script can be used to delete old AMI's and snapshots. This script has been tested with AWS Cloudshell."
+  echo
+  echo "Options:"
+  echo
+  echo -e "  --days-old\t\tThis set the minimmum age in days to delete AMI's."
+  echo
+  echo "Example:"
+  echo
+  echo "  ./delete-all-old-amis.sh"
+  echo
+  echo "Example:"
+  echo
+  echo "  ./delete-all-old-amis.sh --days-old 14"
+}
+
+function log {
+  local -r level="$1"
+  local -r message="$2"
+  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  >&2 echo -e "${timestamp} [${level}] [$SCRIPT_NAME] ${message}"
+}
+
+function log_info {
+  local -r message="$1"
+  log "INFO" "$message"
+}
+
+function log_warn {
+  local -r message="$1"
+  log "WARN" "$message"
+}
+
+function log_error {
+  local -r message="$1"
+  log "ERROR" "$message"
+}
+
 function main {
     local -r days="$1"
     latest_date=$(date  --date="$days days ago" +"%Y-%m-%d")
